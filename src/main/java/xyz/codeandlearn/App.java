a80f74e0615e8f5f61ab5088daf3c7cb72c656fa
Danyvi <vinci.dani@gmail.com>
2025-01-03 19:23:30 +0100

    spring bean definition

--- src/main/java/xyz/codeandlearn/App.java
+++ src/main/java/xyz/codeandlearn/App.java
@@ -1,10 +1,14 @@
 package xyz.codeandlearn;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Developer developer = new Developer();
+        Developer developer = context.getBean(Developer.class);
         developer.buildSoftware();
     }
 }
