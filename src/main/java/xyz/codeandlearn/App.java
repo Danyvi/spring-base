95a5d75525d3320d84330f1f1aa82106de7d33cb
Danyvi <vinci.dani@gmail.com>
2025-01-03 18:41:18 +0100

    init

---
+++ src/main/java/xyz/codeandlearn/App.java
@@ -0,0 +1,10 @@
+package xyz.codeandlearn;
+
+public class App {
+    public static void main(String[] args) {
+
+
+        Developer developer = new Developer();
+        developer.buildSoftware();
+    }
+}
---
+++ src/main/java/xyz/codeandlearn/Developer.java
@@ -0,0 +1,7 @@
+package xyz.codeandlearn;
+
+public class Developer {
+    public void buildSoftware() {
+        System.out.println("I am a developer and I am building software");
+    }
+}
